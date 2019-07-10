2f33a195876e79ff471a068dd836879f1c6226e5
JunyaUkyou <ukyou.junya0514@gmail.com>
2019-07-10 23:56:16 +0900

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import HomeJa from './HomeJa';
-import HomeEN from './HomeEN';
 import { getText } from './HomeText';
 
 class Home extends React.Component {
   render() {
 
     const message = getText(this.props.lang.current);
-    console.log(message);
     const list = message.notice.map((val,index) => <li key={index}>{val}</li>)
 
     return (
